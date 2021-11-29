2e84b6e71b1603be79fbf7b185e8d19a6bfe1562
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2021-11-29 11:42:31 -0300

    removing unnecessary qualifier annotations

--- src/main/kotlin/com/example/quotes/config/QuoteClientMapConfig.kt
+++ src/main/kotlin/com/example/quotes/config/QuoteClientMapConfig.kt
@@ -5,7 +5,6 @@ import com.example.quotes.enums.QuoteType
 import com.example.quotes.enums.QuoteType.CHUCK_NORRIS
 import com.example.quotes.enums.QuoteType.INSPIRATIONAL
 import com.example.quotes.enums.QuoteType.PROGRAMMING
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -13,9 +12,9 @@ import org.springframework.context.annotation.Configuration
 class QuoteClientMapConfig {
     @Bean
     fun quoteClientMap(
-        @Qualifier("programmingQuoteClient") programmingQuoteClient: IQuoteClient,
-        @Qualifier("inspirationalQuoteClient") inspirationalQuoteClient: IQuoteClient,
-        @Qualifier("chuckNorrisQuoteClient") chuckNorrisQuoteClient: IQuoteClient
+        programmingQuoteClient: IQuoteClient,
+        inspirationalQuoteClient: IQuoteClient,
+        chuckNorrisQuoteClient: IQuoteClient
     ): Map<QuoteType, IQuoteClient> = mapOf(
         PROGRAMMING to programmingQuoteClient,
         INSPIRATIONAL to inspirationalQuoteClient,
