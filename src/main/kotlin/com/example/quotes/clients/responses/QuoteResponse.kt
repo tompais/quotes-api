d0a306ff5388ea9e08296f868b201b2fd19eca4a
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2021-11-30 11:18:45 -0300

    removing comma

--- src/main/kotlin/com/example/quotes/clients/responses/QuoteResponse.kt
+++ src/main/kotlin/com/example/quotes/clients/responses/QuoteResponse.kt
@@ -3,5 +3,5 @@ package com.example.quotes.clients.responses
 import com.fasterxml.jackson.annotation.JsonAlias
 
 open class QuoteResponse(
-    @field:JsonAlias("en", "text", "value") val quote: String,
+    @field:JsonAlias("en", "text", "value") val quote: String
 )
