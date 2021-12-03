e646c26393a34d8d254736b983a281372152bc3d
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2021-12-03 15:01:28 -0300

    deleting unnecessary mapper configurer

--- src/main/kotlin/com/example/quotes/config/ObjectMapperConfig.kt
+++ src/main/kotlin/com/example/quotes/config/ObjectMapperConfig.kt
@@ -8,12 +8,10 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies.SNAKE_CASE
 import com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS
 import com.fasterxml.jackson.databind.SerializationFeature.WRITE_ENUMS_USING_TO_STRING
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.jacksonMapperBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import java.util.TimeZone
 
@@ -31,18 +29,6 @@ class ObjectMapperConfig {
         .defaultTimeZone(TimeZone.getTimeZone("UTC"))
         .propertyNamingStrategy(SNAKE_CASE)
         .build()
-
-    @Bean
-    fun jackson2ObjectMapperBuilder(): Jackson2ObjectMapperBuilder =
-        Jackson2ObjectMapperBuilder()
-            .modules(KotlinModule.Builder().build(), JavaTimeModule())
-            .featuresToEnable(WRITE_ENUMS_USING_TO_STRING)
-            .featuresToEnable(READ_ENUMS_USING_TO_STRING)
-            .featuresToEnable(ACCEPT_CASE_INSENSITIVE_ENUMS)
-            .featuresToDisable(WRITE_DATES_AS_TIMESTAMPS)
-            .featuresToDisable(FAIL_ON_UNKNOWN_PROPERTIES)
-            .timeZone(TimeZone.getTimeZone("UTC"))
-            .propertyNamingStrategy(SNAKE_CASE)
 
     @Bean
     fun mappingJackson2HttpMessageConverter(): MappingJackson2HttpMessageConverter =
